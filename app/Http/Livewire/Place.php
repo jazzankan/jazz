03b9c73f720f17a215238f90edceb72774362c53
jazzankan <shasfn@gmail.com>
2021-11-20 17:19:41 +0100

    Startat Livewire. Ska fixa autocomplete för orter och org.

---
+++ app/Http/Livewire/Place.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Place extends Component
+{
+    public function render()
+    {
+        return view('livewire.place');
+    }
+}
