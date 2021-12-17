0adfa41329d92d48c9a486a6518156c570a4e9dd
jazzankan <shasfn@gmail.com>
2021-12-17 10:19:49 +0100

    Skapat Livewire-komponent för att välja artister till ett event.

---
+++ app/Http/Livewire/SelectArtists.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SelectArtists extends Component
+{
+
+
+
+    public function render()
+    {
+        return view('livewire.select-artists');
+    }
+}
