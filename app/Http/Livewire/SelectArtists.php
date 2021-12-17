ed4646a818ec056d7319aa4a02b13064ccab7163
jazzankan <shasfn@gmail.com>
2021-12-17 16:34:39 +0100

    Jobbar med Livewire-komponent för att välja artister till ett event.

--- app/Http/Livewire/SelectArtists.php
+++ app/Http/Livewire/SelectArtists.php
@@ -6,11 +6,17 @@ use Livewire\Component;
 
 class SelectArtists extends Component
 {
+public $query;
+public $artists;
 
-
-
+public function updatedQuery()
+{
+    $this->artists = Artist::where('name','like','%' . $this->query . '%');
+}
     public function render()
     {
-        return view('livewire.select-artists');
+        return view('livewire.select-artists')
+            ->get()
+            ->toArray();
     }
 }
--- resources/views/livewire/select-artists.blade.php
+++ resources/views/livewire/select-artists.blade.php
@@ -1,3 +1,14 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    <label for="artists">Koppla artister:</label><br>
+    <input type="text"
+           class="max-w-lg w-full mt-2 mb-6 px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-green-500"
+           value="" name="name" placeholder="Sök artist..." wire:model="query"/>
+            <ul>
+            @foreach($artists as $artist)
+                <li>{{ $artist->name }}</li>
+            </ul>
+
+    <p>Tillagda artister:<br>
+        Lundström, Tord
+    </p>
 </div>
