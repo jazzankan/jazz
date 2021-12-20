4c03b900d30ed30226266822b7d2a6f2402441f6
jazzankan <shasfn@gmail.com>
2021-12-20 13:08:55 +0100

    Jobbar fortfarande med Livewire-komponent för att välja artister till ett event.

--- app/Http/Livewire/SelectArtists.php
+++ app/Http/Livewire/SelectArtists.php
@@ -3,20 +3,26 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Artist;
 
 class SelectArtists extends Component
 {
 public $query;
 public $artists;
 
+function mount(){
+    $this->query = "";
+    $this->artists = [];
+}
+
 public function updatedQuery()
 {
-    $this->artists = Artist::where('name','like','%' . $this->query . '%');
+    $this->artists = Artist::where('name','like','%' . $this->query . '%')
+        ->get();
+    //dd($this->artists);
 }
     public function render()
     {
-        return view('livewire.select-artists')
-            ->get()
-            ->toArray();
+        return view('livewire.select-artists')->with('artists',$this->artists);
     }
 }
--- resources/views/livewire/select-artists.blade.php
+++ resources/views/livewire/select-artists.blade.php
@@ -3,11 +3,12 @@
     <input type="text"
            class="max-w-lg w-full mt-2 mb-6 px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-green-500"
            value="" name="name" placeholder="Sök artist..." wire:model="query"/>
+            <p>Förslag:</p>
             <ul>
             @foreach($artists as $artist)
                 <li>{{ $artist->name }}</li>
+                @endforeach
             </ul>
-
     <p>Tillagda artister:<br>
         Lundström, Tord
     </p>
