5665a28cae9ec81ccc7fdd7b26d1e0650e01974b
jazzankan <shasfn@gmail.com>
2021-12-28 11:04:19 +0100

    Jobbar fortfarande med Livewire-komponent för att välja artister till ett event. alpine funkar.

--- resources/views/livewire/select-artists.blade.php
+++ resources/views/livewire/select-artists.blade.php
@@ -3,12 +3,13 @@
     <input type="text"
            class="max-w-lg w-full mt-2 mb-6 px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-green-500"
            value="" name="name" placeholder="Sök artist..." wire:model="query"/>
+    <div x-data="{ names:[] }">
     <p>Förslag:</p>
     @if(strlen($query)>2)
         <ul>
             @if(count($artists)> 0)
                 @foreach($artists as $artist)
-                    <li><a href="" id="{{ $artist->id }}" class="text-blue-800">{{ $artist->name }}</a></li>
+                    <li><a x-on:click="names.push('{{ $artist->name }}')" href="#" id="{{ $artist->id }}" class="text-blue-800">{{ $artist->name }}</a></li>
                 @endforeach
             @else
                 <li class="text-red-800 font-bold">Ingen träff!</li>
@@ -16,6 +17,11 @@
             @endif
         </ul>
         <p>Tillagda artister:<br>
-            Lundström, Tord
+            <ul>
+            <template x-for="name in names">
+                <li x-text="name"></li>
+            </template>
+            </ul>
         </p>
+    </div>
 </div>
