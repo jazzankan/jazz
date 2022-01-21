2c6c0f18166a32dc33ae70142de9585558ee65fa
jazzankan <shasfn@gmail.com>
2022-01-21 16:35:56 +0100

    Håller på med val att söka bara kommande konserter. Kämpar förtvivlat.

--- app/Http/Livewire/SearchEvents.php
+++ app/Http/Livewire/SearchEvents.php
@@ -4,30 +4,48 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Event;
+use App\Models\Place;
+//use Illuminate\Support\Facades\DB;
 
 class SearchEvents extends Component
 {
     public $query;
     public $events;
     public $coming;
+    public $places;
 
 
-    function mount(){
+    function mount()
+    {
         $this->query = "";
         $this->events = [];
         $this->coming = 1;
+        $this->places = Place::all();
     }
 
     public function togglecoming()
     {
         $this->coming = 1 - $this->coming;
+        $this->updatedQuery();
     }
+/*->each(function($item){
+    return $item*$item;
+});
+});*/
     //Nedan ska in då att om coming är 1 så bara kommande, annars rubbet.
     public function updatedQuery()
     {
-        $this->events = Event::where('name','like','%' . $this->query . '%')
-            ->get();
+        if ($this->coming === 1) {
+            $this->events = Event::where('name', 'like', '%' . $this->query . '%')
+                ->orWhere($this->places->each(function($item){$item->where($item->municipality, 'like', '%' . $this->query . '%');}))
+                //->orWhere($this->places->each(function($key){$key = 'municipality'}, 'like', '%' . $this->query . '%')
+                ->get();
+        } else {
+            $this->events = Event::where('name', 'like', '%' . $this->query . '%')
+                ->get();
+        }
     }
+
     public function render()
     {
         return view('livewire.search-events');
