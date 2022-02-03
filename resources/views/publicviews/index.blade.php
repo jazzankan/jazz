a56cb60d2abf5c84c4d71d952aa850526df21320
jazzankan <shasfn@gmail.com>
2022-02-03 18:57:11 +0100

    Kolumnerna fixade.

--- resources/views/publicviews/index.blade.php
+++ resources/views/publicviews/index.blade.php
@@ -1,17 +1,17 @@
 <x-guest-layout>
     <div class="bg-blue-100 rounded-lg max-w-screen-xl m-auto">
         <div class="grid grid-cols-1 md:grid-cols-12 md:gap-3">
-            <div class="m-2.5 md:border-r md:border-red-700 md:col-span-3">
-                <h1 class="mv-2 text-3xl text-center">Länkar</h1>
+            <div class="m-2.5 md:border-r md:border-red-700 md:col-span-3 order-2 md:order-1">
+                <h1 class="mv-2 text-3xl text-center">Meddelanden</h1>
             </div>
-            <div class="m-2.5 md:col-span-6">
+            <div class="m-2.5 md:col-span-6 order-1 md:order-2">
                 <h1 class="mv-2 mb-4 text-3xl text-center">Evenemang</h1>
                 <div>
                 @livewire('pub-eventsearch')
                 </div>
         </div>
-        <div class="m-2.5 md:border-l md:border-red-700 md:col-span-3">
-            <h1 class=" mv-2 text-3xl text-center">Meddelanden</h1>
+        <div class="m-2.5 md:border-l md:border-red-700 md:col-span-3 order-3">
+            <h1 class=" mv-2 text-3xl text-center">Länkar</h1>
         </div>
     </div>
     </div>
