0ac6c135e289639dd88d726b3738955d221ce663
jazzankan <shasfn@gmail.com>
2022-04-19 14:30:10 +0200

    Livewiremetod att ändra warning till0 funkar, och raden blir osynlig sen.

--- app/Http/Livewire/CheckSites.php
+++ app/Http/Livewire/CheckSites.php
@@ -9,12 +9,14 @@ use App\Models\SpiderData;
 class CheckSites extends Component
 {
     public $organizers;
+    public $hideid;
 
     function mount()
     {
         $this->organizers = Organizer::whereHas('spiderdata', function ($q) {
             $q->where('warning', '1');
         })->get();
+        $this->hideid = 0;
     }
 
     function clearsite($orgid)
@@ -22,10 +24,11 @@ class CheckSites extends Component
         $spiderdata = SpiderData::where('organizer_id',$orgid)->first();
         $spiderdata->warning = 0;
         $spiderdata->save();
+        $this->hideid = $orgid;
     }
 
     public function render()
     {
-        return view('livewire.checksites');
+        return view('livewire.checksites')->with('hideid',$this->hideid);
     }
 }
--- resources/views/livewire/checksites.blade.php
+++ resources/views/livewire/checksites.blade.php
@@ -1,5 +1,5 @@
 <div class="max-w-screen-lg mx-auto sm:px-6 lg:px-8">
     @foreach($organizers as $o)
-        <p class="py-2">{{ $o->orgname }} <span wire:click="clearsite( {{ $o->id }})" class="btn-small cursor-pointer">Klar</span></p>
+        @if($o->spiderdata->warning == 1 && $hideid != $o->id)<p id="{{ $o->id }}" class="py-2"  >{{ $o->orgname }} <span wire:click="clearsite({{ $o->id }})" class="btn-small cursor-pointer">Klar</span></p>@endif
     @endforeach
 </div>
