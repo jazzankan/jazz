f134157aa68dfdceff3cef69d557eb89a8f8f5a3
jazzankan <shasfn@gmail.com>
2022-04-20 16:04:04 +0200

    Håller på att fixa till spider_data. Ny kolumn in (dayinterval) och två bort (fixdate och comment)

--- resources/views/livewire/checksites.blade.php
+++ resources/views/livewire/checksites.blade.php
@@ -1,5 +1,13 @@
 <div class="max-w-screen-lg mx-auto sm:px-6 lg:px-8">
-    @foreach($organizers as $o)
-        @if($o->spiderdata->warning == 1 && $hideid != $o->id)<p id="{{ $o->id }}" class="py-2"  >{{ $o->orgname }} <span wire:click="clearsite({{ $o->id }})" class="btn-small cursor-pointer">Klar</span></p>@endif
-    @endforeach
+    @if(count($organizers)>0)
+        @foreach($organizers as $o)
+            @if($o->spiderdata->warning == 1 && $hideid != $o->id)
+                <p id="{{ $o->id }}" class="py-2">{{ $o->orgname }} <span wire:click="clearsite({{ $o->id }})"
+                                                                          class="btn-small cursor-pointer">Klar</span>
+                </p>
+            @endif
+        @endforeach
+    @else
+        <h2 class="text-lg">Allt är kollat!<h2>
+    @endif
 </div>
