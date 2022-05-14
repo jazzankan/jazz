4e39d8afa3775b0a2097a4fd199ab82edb2a5adc
jazzankan <shasfn@gmail.com>
2022-05-14 16:58:06 +0200

    Skapat aboutsida. Skrivit text. Kontaktsida ska skapas.

--- resources/views/publicviews/about.blade.php
+++ resources/views/publicviews/about.blade.php
@@ -1,3 +1,21 @@
 <x-guest-layout>
-    <div>Hej!</div>
+    <div class="md:ml-60">
+        <h2 class="text-2xl">Hej!</h2>
+        <p class="max-w-prose">
+            Jazztider listar kommande jazzkonserter i Sverige. Alla? Nej, glöm det! Jag är ensam om att sköta den här
+            sajten,
+            vilket sätter gränser. Vita fläckar finns det många på min karta! Jag är själv bosatt utanför Stockholm,
+            så det kan säkert bli geografisk slagsida också. Jag listar helt enkelt det jag hittar och som jag bedömer
+            som
+            relevant. Och vad är jazz? Just i den här kontexten är det jag (igen) som bestämmer det. Annars kan man ha
+            långa diskussioner om saken.
+            Vissa vänner av improvisationsmusik kan rent av ogilla jazzbegreppet. Det förstår jag, men väljer ändå att
+            använda det, förhoppningsvis
+            på ett inte alltför inskränkande sätt.
+            Du kan hjälpa mig eller påverka mig genom att tipsa, föreslå, uppmuntra, rätta eller klaga i
+            <a class="text-blue-800" href="/contact">kontaktformuläret.</a>
+        <p class="mt-6">Hälsningar
+            Anders Fredriksson
+        </p>
+    </div>
 </x-guest-layout>
