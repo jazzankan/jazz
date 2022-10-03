235ff429782e48ca4ba9b808aaaca0c7d02e0b79
jazzankan <shasfn@gmail.com>
2022-10-03 18:08:09 +0200

    Börjat på Hamburgare.

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,14 @@
 <header>
-<div class="flex h-20 bg-gradient-to-r from-blue-100 via-blue-200 to-blue-500 md:bg-instr-pattern  md:h-32 mt-0">
-    <p class="truncate m-auto align-middle text-red-600 ml-2 md:ml-28">
-        <span class="text-4xl leading-relaxed md:leading-loose md:text-6xl" style="font-family: 'Gloria Hallelujah',cursive !important;"><a href="/">Jazztider</a></span><span class="ml-6 font-bold md:text-2xl">Konserter i Sverige</span></p>
-</div>
+    <div class="flex h-20 bg-gradient-to-r from-blue-100 via-blue-200 to-blue-500 md:bg-instr-pattern  md:h-32 mt-0">
+        <p class="truncate m-auto align-middle text-red-600 ml-2 md:ml-28">
+            <span class="text-4xl leading-relaxed md:leading-loose md:text-6xl"
+                  style="font-family: 'Gloria Hallelujah',cursive !important;"><a href="/">Jazztider</a></span><span
+                class="ml-6 font-bold md:text-2xl">Konserter i Sverige</span>
+            <span class="text-black text-sm md:hidden">
+            <a href="#tips">Tips</a>
+            <a href="#länkar">Länkar</a>
+        </span></p>
+
+    </div>
 </header>
 
