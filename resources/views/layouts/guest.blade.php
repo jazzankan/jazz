f0572a7532cb4dd56a187d3be8ef318729778002
jazzankan <shasfn@gmail.com>
2022-12-29 17:57:38 +0100

    Fixat med nyckelord i den publika layouten.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="keywords" content="jazz, Sverige, jazzkonserter, konserter, spelningar, jazzgig">
 
         <title>{{ config('app.name', 'Jazztider') }}</title>
 
