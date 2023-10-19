66e5b20e30136be2c494081df724aabf486f6379
jazzankan <shasfn@gmail.com>
2023-10-19 18:42:51 +0200

    Redigering av tips funkar.

--- resources/views/tips/edit.blade.php
+++ resources/views/tips/edit.blade.php
@@ -14,31 +14,38 @@
                         <div>
                             <label for="headline">Rubrik:</label><br>
                             <input type="text"
+                                   id="headline"
                                    class="max-w-lg w-full mt-2 mb-6 px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-green-500"
                                    value="{{ $tip->headline }}" name="headline"/>
                         </div>
                         <div>
                             <label for="body">Text:</label><br>
                             <input type="text"
+                                   id="body"
                                    class="max-w-lg w-full mt-2 mb-6 px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-green-500"
                                    value="{{ $tip->body }}" name="body"/>
                         </div>
                         <div>
                             <label for="link">Länk:</label><br>
                             <input type="text"
+                                   id="link"
                                    class="max-w-lg w-full mt-2 mb-6 px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-green-500"
                                    value="{{ $tip->link }}" name="link"/>
-
                         </div>
                         <div>
-                            <label for="shownr">Visningsprio:</label>
+                            <label for="shownr">Visningsprio:</label><br>
+                            <select name="shownr" id="shownr" class="border rounded-lg">
+                                <option value=1 @selected($tip->shownr == 1)>1</option>
+                                <option value=2 @selected($tip->shownr == 2)>2</option>
+                                <option value=3 @selected($tip->shownr == 3)>3</option>
+                            </select>
                         </div>
-                        <div class="mt-4"><label for="day">Startdatum:</label><br>
-                            <input type="date" class="border rounded-lg mb-6"
+                        <div class="mt-4"><label for="start">Startdatum:</label><br>
+                            <input type="date" id="start" class="border rounded-lg mb-6"
                                    value="{{ $tip->pubstart != null ? $tip->pubstart : ''}}" name="pubstart">
                         </div>
-                        <div class="mt-1"><label for="day">Slutdatum (lämna blank om evig):</label><br>
-                            <input type="date" class="border rounded-lg mb-6"
+                        <div class="mt-1"><label for="stop">Slutdatum (lämna blank om evig):</label><br>
+                            <input type="date" id="stop" class="border rounded-lg mb-6"
                                    value="{{ $tip->pubstop != null ? $tip->pubstop : ''}}" name="pubstop">
                         </div>
                         <div class="form-group">
@@ -52,7 +59,6 @@
                 </form>
             </div>
             <div>
-                <p>
                 @if ($errors->any())
                     <div class="text-red-600 bg-pink-200 mt-2 py-4 pl-2">
                         <ul>
@@ -62,7 +68,6 @@
                         </ul>
                     </div>
                     @endif
-                    </p>
             </div>
         </div>
     </div>
