448b6d6dd77521dae0f33533b325c0fc6969bf0b
anders <anders@webbsallad.se>
2025-08-21 13:58:26 +0200

    Lagt till from i Contactmessage.php

--- app/Notifications/Contactmessage.php
+++ app/Notifications/Contactmessage.php
@@ -41,6 +41,7 @@ class Contactmessage extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
+                    ->from('jazztider@webbsallad.se', 'Jazztider')
                     ->subject('Jazztiders kontaktformulär')
                     ->greeting('Hallå Anders!')
                     ->line('Jazztider har kontaktats av ' . $this->Contactmessage['name'])
